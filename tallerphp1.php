2f6543ac85687a571021a34c632c98f27bf7893b
jorgsil1 <86447583+jorgsil1@users.noreply.github.com>
2021-08-10 17:36:57 -0500

    Add files via upload

---
+++ tallerphp1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php
+ echo "¡Hola, soy un script de PHP!";
+ ?>
+ <?php
+echo "primer curso de codigo en php fagaro<br >";
+echo "materiales y ensayos<br >";
+echo "12345<br >";
+?>
+<?php
+Print "PROGRAMACION EL MEJOR<br >";
+?>
+
+<?php
+$numero=50;
+$texto="<br> esto es un texto";
+$boolean=true;
+
+echo $numero;
+echo $texto."<br>";
+echo $boolean;
+define("saludo", "<br> Hola como estas?<br>");
+define("numero",20);
+echo saludo."tengo".numero."anos";
+?>
+</body>
+</html>
